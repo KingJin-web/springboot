a503876c13646bb270c4bffcd566c3618769d9ee
KingJin-web <1261337209@qq.com>
2021-08-25 03:17:44 +0800

    ✔ mybatis—plus

---
+++ mybatis_plus/src/test/java/com/king/mybatis_plus/MybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.mybatis_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
