43cb9f2456000350b3948393899147079c3c59d0
KingJin-web <1261337209@qq.com>
2021-10-24 21:31:13 +0800

    ✔ SpringBoot整合mybatis(使用xml)

---
+++ mybatis_xml/src/main/java/com/king/mybatis_xml/MybatisXmlApplication.java
@@ -0,0 +1,13 @@
+package com.king.mybatis_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisXmlApplication.class, args);
+    }
+
+}
---
+++ mybatis_xml/src/test/java/com/king/mybatis_xml/MybatisXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.mybatis_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
