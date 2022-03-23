f0afea63fc381b44405b1b035f8a3a9d1768294b
KingJin-web <1261337209@qq.com>
2022-03-23 16:19:44 +0800

    ✔ 登录注册demo

--- security_customize/src/main/java/com/king/security/config/login/DefaultAuthenticationFailureHandler.java
+++ security_customize/src/main/java/com/king/security/config/login/DefaultAuthenticationFailureHandler.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.king.security.vo.ResultObj;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -25,12 +26,26 @@ public class DefaultAuthenticationFailureHandler extends SimpleUrlAuthentication
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException,
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException e) throws IOException,
             ServletException {
-        logger.info("login in failure : " + exception.getMessage());
-
+        logger.info("login in failure : " + e.getMessage());
+        ResultObj resultObj = new ResultObj();
+        if (e instanceof LockedException) {
+            resultObj = ResultObj.error("账户被锁定，登录失败！");
+        } else if (e instanceof BadCredentialsException) {
+            resultObj = ResultObj.error("账户名或密码输入错误，登录失败！");
+        } else if (e instanceof DisabledException) {
+            resultObj = ResultObj.error("账户被禁用，登录失败！");
+        } else if (e instanceof AccountExpiredException) {
+            resultObj = ResultObj.error("账户已过期，登录失败！");
+        } else if (e instanceof CredentialsExpiredException) {
+            resultObj = ResultObj.error("账户已过期，登录失败！");
+        } else {
+            resultObj = ResultObj.error(e.getMessage());
+        }
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(JSON.toJSONString(ResultObj.error(exception.getMessage())));
+        response.getWriter().write(JSON.toJSONString(resultObj));
     }
 
 
