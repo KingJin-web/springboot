280c01a5a3e2a9ff1b233bb1ced36546f097407c
KingJin-web <1261337209@qq.com>
2022-07-26 12:52:10 +0800

    first commit

---
+++ ip/src/main/java/com/king/ip/IpApplication.java
@@ -0,0 +1,13 @@
+package com.king.ip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpApplication.class, args);
+    }
+
+}
---
+++ ip/src/test/java/com/king/ip/IpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.ip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
