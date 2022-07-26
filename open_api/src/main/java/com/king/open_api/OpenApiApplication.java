6533b94ca46a30d9839d892d96413e98be2ca2f1
KingJin-web <1261337209@qq.com>
2022-07-26 21:47:02 +0800

    first commit

---
+++ open_api/src/main/java/com/king/open_api/OpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.king.open_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiApplication.class, args);
+    }
+
+}
---
+++ open_api/src/test/java/com/king/open_api/OpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.open_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
